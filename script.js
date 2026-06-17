127b3b6f94ea4e7f7032a32e650b74f1e9f4aecd
eduardakaufmannpetry-jpg <eduarda.kaufmann.petry@escola.pr.gov.br>
2026-06-17 11:16:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const formulario = document.querySelector("form");
 
+formulario.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const nome = document.getElementById("nome").value;
+    const email = document.getElementById("email").value;
+    const mensagem = document.getElementById("mensagem").value;
+
+    if (nome === "" || email === "" || mensagem === "") {
+        alert("Por favor, preencha todos os campos.");
+    } else {
+        alert("Obrigado pela sua mensagem, " + nome + "!");
+        formulario.reset();
+    }
+});
